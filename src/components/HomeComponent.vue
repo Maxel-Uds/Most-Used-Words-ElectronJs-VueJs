2aeb01fbada53b4e2403e828bdf65bd99a1f83ad
Maxel-Uds <maxellopes32@gmail.com>
2023-01-20 22:00:37 -0300

    Interface Gráfica com Vue JS #03

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,5 +1,9 @@
 <template>
   <v-container fluid>
+    <v-form>
+      <v-file-input multiple chips v-model="files" label="Selecione as legendas..." prepend-icon="mdi-message-text" outlined append-icon="mdi-send" @click:append="processSubtitles"/>
+    </v-form>
+
     <div class="pills">
       <PillComponent v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount"/>
     </div>
@@ -12,6 +16,7 @@ import PillComponent from './PillComponent'
 export default {  
   data: function() {
     return {
+      files: [],
       groupedWords: [
         { name: 'you', amount: 900 },
         { name: 'he', amount: 600 },
@@ -21,6 +26,11 @@ export default {
   },
   components: {
     PillComponent
+  },
+  methods: {
+    processSubtitles() {
+      alert(this.files)
+    }
   }
 }
 </script>
