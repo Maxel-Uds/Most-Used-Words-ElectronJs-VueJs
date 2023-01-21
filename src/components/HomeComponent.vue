e7746a94fa8dfcaf17988a9371471b909a07b5a4
Maxel-Uds <maxellopes32@gmail.com>
2023-01-20 21:36:32 -0300

    Interface Gráfica com Vue JS #01

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fluid>
+    <div class="pills">
+      <PillComponent v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount"/>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import PillComponent from './PillComponent'
+
+export default {  
+  data: function() {
+    return {
+      groupedWords: [
+        { name: 'you', amount: 900 },
+        { name: 'he', amount: 600 },
+        { name: 'i', amount: 700 },
+      ]
+    }
+  },
+  components: {
+    PillComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
